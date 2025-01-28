ab9dacd1115279ce331d97b1ad26ea4bb4d3c128
Syed Muhammad Murtaza <smmhj19@gmail.com>
2025-01-28 20:12:34 +0500

    Header Section Added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,12 +2,13 @@ import type { Metadata } from "next";
 import { DM_Sans } from "next/font/google";
 import "./globals.css";
 import clsx from "clsx";
+import {twMerge} from "tailwind-merge";
 
 const dmSans = DM_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Light Saas Landing Page",
-  description: "Template created by Frontend Tribe",
+  title: "Cloud Sphere Saas-Landing Page",
+  description: "Designed & Developed By Syed Murtaza",
 };
 
 export default function RootLayout({
@@ -17,7 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="relative">
-      <body className={clsx(dmSans.className, "antialiased bg-[#EAEEFE]")}>
+      <body className={twMerge(dmSans.className, "antialiased bg-[#EAEEFE]")}>
         {children}
       </body>
     </html>
